0d4d950349961cdd5e4e0e3cf4ceb7efc7e553b2
TabooHarmony <87551761+TabooHarmony@users.noreply.github.com>
2022-12-29 12:34:45 -0500

    ghostkiller
    Roblox Ghost Process Killer Rewrite

---
+++ gkill.py
@@ -0,0 +1,19 @@
+import math
+import os
+import time
+import psutil as psutil
+while True:
+    try:
+        os.system("cls")
+        print("----------------------------------------------------------------")
+        for pid in psutil.process_iter():
+           if(pid.name() == "RobloxPlayerBeta.exe"):
+               print(str(pid.pid)+" mem:"+str(math.trunc(psutil.Process(pid.pid).memory_info().rss / 1024 / 1024))+"M Status:"+psutil.Process(pid.pid).status())
+        for pid in psutil.process_iter():
+           if(pid.name() == "RobloxPlayerBeta.exe"):
+               if (psutil.Process(pid.pid).memory_info().rss / 1024 / 1024)<50 and psutil.Process(pid.pid).status()== "running":
+                   psutil.Process(pid.pid).suspend()
+                   print(str(pid.pid)+" suspended")
+        time.sleep(5)
+    except:
+        pass
